14320a1db1808d38bf838fc221ca028196f8a0c8
Magnus-github <magnusrubentibbe@MacNussBook-Pro-4.local>
2024-09-15 14:53:44 +0200

    Added more satellite data

--- src/cassini/create_dashboard.py
+++ src/cassini/create_dashboard.py
@@ -13,11 +13,14 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
 from src.cassini.get_IOT_data import get_time_series as iot_time_series
-from src.cassini.open_tif import get_time_series as sat_time_series
+from src.cassini.get_sat_data import get_time_series as sat_time_series
+from src.cassini.get_sat_data import update_sat_data
+
 
 
 SAT_DATA_DIR = "satellite_data"
-BANDS = ["CO", "NO2", "O3", "CH4"]
+BANDS = ["CO", "NO2", "O3", "SO2"]
+POSITIONS = [(50.087,  14.424)]
 
 
 def create_dashboard(sat_data_dir):
@@ -36,7 +39,7 @@ def create_dashboard(sat_data_dir):
                 dbc.Col(dbc.Button("CO Level (Satellite)", id="btn-co", color="secondary", className="m-1"), width="auto"),
                 dbc.Col(dbc.Button("NO2 Level (Satellite)", id="btn-no2", color="secondary", className="m-1"), width="auto"),
                 dbc.Col(dbc.Button("O3 Level (Satellite)", id="btn-o3", color="secondary", className="m-1"), width="auto"),
-                dbc.Col(dbc.Button("CH4 Level (Satellite)", id="btn-ch4", color="secondary", className="m-1"), width="auto"),
+                dbc.Col(dbc.Button("SO2 Level (Satellite)", id="btn-SO2", color="secondary", className="m-1"), width="auto"),
             ], className="mb-4"),
             dcc.Graph(id="graph-content", style={"height": "80vh"})
         ], fluid=True, className="px-4 py-3")
@@ -48,8 +51,14 @@ def create_dashboard(sat_data_dir):
         interval=20*60*1000,  # in milliseconds, 20 minutes
         n_intervals=0
     ))
+    # update sat data every 24 hours
+    app.layout.children[0].children.append(dcc.Interval(
+        id='sat-interval-component',
+        interval=24*60*60*1000,  # in milliseconds, 24 hours
+        n_intervals=0
+    ))
 
-    def get_updated_data():
+    def _get_updated_data():
         time_coarse, time_fine, time_co2, temperature, humidity, pressure, rainfall, co2 = iot_time_series()
 
         sat_dates = []
@@ -72,6 +81,11 @@ def create_dashboard(sat_data_dir):
                  "satellite": {band: (val, t) for band, val, t in zip(BANDS, sat_vals, sat_dates)}}
         
         return output
+    
+    def _update_sat_data():
+        for pos in POSITIONS:
+            for band in BANDS:
+                update_sat_data(pos, band)
 
     @app.callback(
         [Output("graph-content", "figure"),
@@ -84,7 +98,7 @@ def create_dashboard(sat_data_dir):
          Output("btn-co", "color"),
          Output("btn-no2", "color"),
          Output("btn-o3", "color"),
-         Output("btn-ch4", "color")],
+         Output("btn-SO2", "color")],
         [Input("btn-overview", "n_clicks"),
          Input("btn-temperature", "n_clicks"),
          Input("btn-humidity", "n_clicks"),
@@ -94,8 +108,9 @@ def create_dashboard(sat_data_dir):
          Input("btn-co", "n_clicks"),
          Input("btn-no2", "n_clicks"),
          Input("btn-o3", "n_clicks"),
-         Input("btn-ch4", "n_clicks"),
-         Input("interval-component", "n_intervals")],
+         Input("btn-SO2", "n_clicks"),
+         Input("interval-component", "n_intervals"),
+         Input("sat-interval-component", "n_intervals")],
          prevent_initial_call=False
     )
     def update_graph(*args):
@@ -106,7 +121,7 @@ def create_dashboard(sat_data_dir):
             button_id = ctx.triggered[0]["prop_id"].split(".")[0]
 
         # Get updated data
-        data_dict = get_updated_data()
+        data_dict = _get_updated_data()
 
         subplot_titles = list(data_dict["station_out"].keys()) + list(data_dict["station_in"].keys()) + [key + " (Satellite)" for key in data_dict["satellite"].keys()]
 
@@ -143,7 +158,7 @@ def create_dashboard(sat_data_dir):
             fig.update_yaxes(title_text="CO Level", row=2, col=3)
             fig.update_yaxes(title_text="NO2 Level", row=3, col=1)
             fig.update_yaxes(title_text="O3 Level", row=3, col=2)
-            fig.update_yaxes(title_text="CH4 Level", row=3, col=3)
+            fig.update_yaxes(title_text="SO2 Level", row=3, col=3)
         elif button_id == "btn-temperature":
             t = data_dict["station_out"]["Temperature"][1]
             val = data_dict["station_out"]["Temperature"][0]
@@ -184,17 +199,17 @@ def create_dashboard(sat_data_dir):
             val = data_dict["satellite"]["O3"][0]
             fig = go.Figure(go.Scatter(x=t, y=val, name="O3 Level (Satellite)"))
             fig.update_layout(title="O3 Level (Satellite)", xaxis_title="Time", yaxis_title="O3 Level")
-        elif button_id == "btn-ch4":
-            t = data_dict["satellite"]["CH4"][1]
-            val = data_dict["satellite"]["CH4"][0]
-            fig = go.Figure(go.Scatter(x=t, y=val, name="CH4 Level (Satellite)"))
-            fig.update_layout(title="CH4 Level (Satellite)", xaxis_title="Time", yaxis_title="CH4 Level")
+        elif button_id == "btn-SO2":
+            t = data_dict["satellite"]["SO2"][1]
+            val = data_dict["satellite"]["SO2"][0]
+            fig = go.Figure(go.Scatter(x=t, y=val, name="SO2 Level (Satellite)"))
+            fig.update_layout(title="SO2 Level (Satellite)", xaxis_title="Time", yaxis_title="SO2 Level")
 
         fig.update_layout(height=700)
 
         # Update button colors
         button_colors = ["secondary"] * 10
-        button_index = ["btn-overview", "btn-temperature", "btn-humidity", "btn-pressure", "btn-rainfall", "btn-co2", "btn-co", "btn-no2", "btn-o3", "btn-ch4"].index(button_id)
+        button_index = ["btn-overview", "btn-temperature", "btn-humidity", "btn-pressure", "btn-rainfall", "btn-co2", "btn-co", "btn-no2", "btn-o3", "btn-SO2"].index(button_id)
         button_colors[button_index] = "primary"
 
         return fig, *button_colors
--- src/cassini/get_sat_data.py
+++ src/cassini/get_sat_data.py
@@ -1,17 +1,24 @@
 import openeo
 import datetime
 import os
-from info import get_all_updates, create_bounding_box
+import rasterio
+from rasterio.plot import show
+
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
+from src.cassini.info import get_all_updates, create_bounding_box
 
 
 SATELLITE_DATA_DIR = "satellite_data"
 
 def download_sat_data(dt: datetime.datetime, bb: list[float], band: str):
+    print("Downloading satellite data for band", band, "at", dt)
     connection = openeo.connect("https://openeo.dataspace.copernicus.eu/openeo/1.2")
     connection.authenticate_oidc()
 
-    startdate_str = dt
-    enddate_str = dt + datetime.timedelta(days=1)
+    startdate_str = dt - datetime.timedelta(hours=1)
+    enddate_str = startdate_str + datetime.timedelta(days=1)
     startdate_str = startdate_str.strftime("%Y-%m-%d")
     enddate_str = enddate_str.strftime("%Y-%m-%d")
     temporal_extent = [startdate_str, enddate_str]
@@ -27,11 +34,12 @@ def download_sat_data(dt: datetime.datetime, bb: list[float], band: str):
     result = cube.save_result("GTiff")
     job  = result.create_job()
     job.start_and_wait()
-    output_folder = os.path.join("output", "output_" + dt.strftime("%Y-%m-%d-%H-%M") + "_" + band)
+    output_folder = os.path.join(SATELLITE_DATA_DIR, "output_" + dt.strftime("%Y-%m-%d-%H-%M") + "_" + band)
     job.get_results().download_files(output_folder)
 
 
 def update_sat_data(positions: list[tuple[float, float]], band: str):
+    print("Updating satellite data for band", band)
     if not os.path.exists(SATELLITE_DATA_DIR):
         os.makedirs(SATELLITE_DATA_DIR)
 
@@ -45,13 +53,12 @@ def update_sat_data(positions: list[tuple[float, float]], band: str):
 
         from_date = latest_date
 
-    diff_to_now = (datetime.datetime.now() - latest_date).days
+    diff_to_now = (datetime.datetime.now() - from_date).days
 
 
     updates_all = {}
 
     for i in range(diff_to_now):
-        from_date += datetime.timedelta(days=1)
         updates = get_all_updates(positions, from_date, width=10)
 
         for pos in positions:
@@ -62,16 +69,66 @@ def update_sat_data(positions: list[tuple[float, float]], band: str):
             updates_all[pos] = list(set(updates_all[pos]))
             updates_all[pos].sort()
 
+        from_date += datetime.timedelta(days=1)
+
     for pos in positions:
         bb = create_bounding_box(pos[0], pos[1], 10)
         for dtime in updates_all[pos]:
+            if datetime.datetime.now() - dtime < datetime.timedelta(days=1):
+                continue
             download_sat_data(dtime, bb, band)
-    
+
+
+def open_with_rasterio(file_path):
+    img = rasterio.open(file_path)
+    arr = img.read()
+    # show(img)
+
+    return arr[0][1,1]
+
+def get_time_series(folder: str, band: str):
+    t = []
+    val = []
+    subdirs_band = [subdir for subdir in os.listdir(folder) if subdir.split("_")[2] == band]
+    for dir in sorted(subdirs_band):
+        date = dir.split("_")[1]
+        if t:
+            current = t[-1]
+            while datetime.datetime.strptime(current, "%Y-%m-%d-%H-%M") < datetime.datetime.strptime(date, "%Y-%m-%d-%H-%M"):
+                current = datetime.datetime.strptime(current, "%Y-%m-%d-%H-%M") + datetime.timedelta(hours=1)
+                current = current.strftime("%Y-%m-%d-%H-%M")
+                t.append(current)
+                val.append(val[-1])
+        t.append(date)
+
+        for file in os.listdir(os.path.join(folder, dir)):
+            if file.endswith(".tif"):
+                file_path = os.path.join(folder, dir, file)
+                co_center = open_with_rasterio(file_path)
+                val.append(abs(co_center))
+    return t, val
+
+
+def plot_time_series(t, val):
+    plt.figure(figsize=(12, 6))
+    plt.plot(t, val)
+    ax=plt.gca()
+    loc = ticker.MultipleLocator(base=26)
+    ax.xaxis.set_major_locator(loc)
+    plt.xlabel("Time")
+    plt.xticks(rotation=25)
+    plt.ylabel("CO Level")
+    plt.tight_layout()
+    plt.show()
 
         
 
 if __name__ == "__main__":
-    positions =  [(50.087,  14.424)]
-    band = "CO"
-    
-    update_sat_data(positions, band)
+    # positions =  [(50.087,  14.424)]
+    # bands = ["CO", "NO2", "O3", "SO2"]
+
+    # for band in bands:
+    #     update_sat_data(positions, band)
+
+    t,  val = get_time_series(SATELLITE_DATA_DIR, "SO2")
+    plot_time_series(t, [abs(v) for v in val])
